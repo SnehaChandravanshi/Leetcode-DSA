1833d2ecc7b5736f1f66c04d2e18ced271781235
Sneha Chandravanshi <161471021+SnehaChandravanshi@users.noreply.github.com>
2026-02-13 16:40:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (30.75%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -1,21 +1,15 @@
-// ✅ Approach 1: Extra Space (Preorder Traversal)
 class Solution {
+    TreeNode prev = null;
     public void flatten(TreeNode root) {
-        if (root == null) return;
-        List<TreeNode> nodes = new ArrayList<>();
-        preorder(root, nodes);
-
-        for (int i = 0; i < nodes.size() - 1; i++) {
-            nodes.get(i).left = null;
-            nodes.get(i).right = nodes.get(i + 1);
+        if(root==null)
+        {
+            return;
         }
-    }
-
-    private void preorder(TreeNode node, List<TreeNode> nodes) {
-        if (node == null) return;
-        nodes.add(node);
-        preorder(node.left, nodes);
-        preorder(node.right, nodes);
+        flatten(root.right);
+        flatten(root.left);
+        root.right=prev;
+        root.left=null;
+        prev=root;
     }
 }
 
